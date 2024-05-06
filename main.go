5a86da55e6b837af237293a935d1cc7c409b8664
Gonzalo <gonzaescobar26@gmail.com>
2024-05-06 19:34:29 -0300

    intento 1 despliegue

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func main() {
-	fmt.Println("Hello world")
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+	
+	app.Static("/", "./client/dist")
+
+
+	app.Listen(":3000")
+	fmt.Println("Server on port 3000")
+	
 }
